1f12f6465403edf578b8d87ae1c1c5b7e684b709
Tomoko-hub <haisaitomoco@gmail.com>
2022-09-08 07:42:29 +0300

    add logout button

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from 'react-router-dom';
 
-const Logout = () => {
+const Logout = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const logoutButton = () =>{
+    signOut(auth).then(()=> {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate("/login");
+    });
+  };
+
   return (
     <div>
-      Logout
+      <p>
+        Logout
+      </p>
+      <button onClick={logoutButton}>
+        Logout
+      </button>
     </div>
   )
 }
