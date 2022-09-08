42b36b03f19aaa1b070930c15de01c99b81633ed
Tomoko-hub <haisaitomoco@gmail.com>
2022-09-08 07:10:05 +0300

    add login with google function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from "./components/Home";
+import { useState } from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreatePost from './components/CreatePost';
@@ -8,13 +9,14 @@ import Logout from './components/Logout';
 import NavBar from './components/NavBar';
 
 function App() {
+  const [ isAuth, setIsAuth ] = useState(false);
   return (
     <Router>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
         <Route path="/logout" element={<Logout />}></Route>
       </Routes>
     </Router>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../firebase";
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const loginWithGoogleAccount = () =>{
+    signInWithPopup(auth, provider).then((result)=> {
+      localStorage.setItem("isAuth", true);
+        setIsAuth(true);
+        navigate("/");
+      });
+  }
+
   return (
     <div>
-      Login
+      <p>
+        Login
+      </p>
+      <button onClick={loginWithGoogleAccount}>
+        Login with Googole Account
+      </button>
     </div>
   )
 }
