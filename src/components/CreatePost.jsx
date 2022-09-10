6486b9fd8dd1a66dd6808ef9a43828d6de5e703b
Tomoko-hub <haisaitomoco@gmail.com>
2022-09-10 08:53:17 +0300

    add useState to createPost component

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./CreatePost.css";
 
 const CreatePost = () => {
+
+  const [ titile, setTitle ] = useState([]);
+  const [ postText, setPostText ] = useState([]);
+
+  const createPost = () => {
+    console.log(titile);
+    console.log(postText);
+  } ;
+
   return (
     <div className='createPostPage'>
       <div className='postContainer'>
         <h1>Create Post</h1>
         <div className="inputPost">
           <div>Titile</div>
-          <input type="text" placeholder='Write title' />
+          <input 
+            type="text" 
+            placeholder='Write title' 
+            onChange={(event) => setTitle(event.target.value)}
+           />
         </div>
         <div className="inputPost">
           <div>Blog</div>
-          <textarea placeholder='Write your blog' />
+          <textarea 
+            placeholder='Write your blog'
+            onChange={(event) => setPostText(event.target.value)}
+           />
         </div>
-        <button className='postButton'>Post your blog</button>
+        <button 
+          className='postButton'
+          onClick={createPost}
+        >Post your blog</button>
       </div>
     </div>
   )
